2317560413b97c7f085e7019fa002dd4aa10a31e
justin@archbox <driftpattern@gmail.com>
2023-08-22 14:52:46 -0700

    added stored procedures for requirements A thru D

--- sba_sql_justin_g.sql
+++ sba_sql_justin_g.sql
@@ -1,17 +1,14 @@
--- create "cms" db if it doesn't exist
-SELECT SCHEMA_NAME
-FROM INFORMATION_SCHEMA.SCHEMATA
-WHERE SCHEMA_NAME = 'cms';
+-- drop the "cms" database if it exists
+DROP DATABASE IF EXISTS cms;
 
-IF FOUND_ROWS() = 0 THEN
-    SOURCE CMS_Database.sql;
-END IF;
+-- build "cms" database from provided CMS_Database.sql
+SOURCE CMS_Database.sql;
 
 
 
--- create stored procedures
+-- create stored procedures to fullfill each requiment
 
--- procedure for requirement A: List Department Courses
+-- requirement A: List Department Courses
 DELIMITER $$
 CREATE PROCEDURE ListDepartmentCourses()
 BEGIN
@@ -26,10 +23,83 @@ $$
 DELIMITER ;
 
 
+-- requirement B: List Most Popular Courses
+DELIMITER $$
+CREATE PROCEDURE ListMostPopularCourses()
+BEGIN
+    -- course name and number of students in each course
+    SELECT c.name AS "Course Name", COUNT(sc.studentId) AS "# Students"
+    FROM course AS c
+    LEFT JOIN studentCourse AS sc ON c.id = sc.courseId
+    GROUP BY c.id
+    ORDER BY "# Students" DESC, c.name ASC;
+END;
+$$
+DELIMITER ;
+
+
+-- requirement C-1: List Courses with No Faculty
+DELIMITER $$
+CREATE PROCEDURE ListCoursesNoFaculty()
+BEGIN
+    -- course names with no faculty
+    SELECT c.name AS "Course Name"
+    FROM course AS c
+    LEFT JOIN facultyCourse AS fc ON c.id = fc.courseId
+    WHERE fc.facultyId IS NULL
+    ORDER BY c.name ASC;
+END;
+$$
+DELIMITER ;
+
+
+-- requirement C-2: List Course Names and Student Count (No Faculty)
+DELIMITER $$
+CREATE PROCEDURE ListCourseStudentCountNoFaculty()
+BEGIN
+    -- course names and # of students with no faculty
+    SELECT c.name AS "Course Name", COUNT(sc.studentId) AS "# Students"
+    FROM course AS c
+    LEFT JOIN studentCourse AS sc ON c.id = sc.courseId
+    LEFT JOIN facultyCourse AS fc ON c.id = fc.courseId
+    WHERE fc.facultyId IS NULL
+    GROUP BY c.id
+    ORDER BY "# Students" DESC, c.name ASC;
+END;
+$$
+DELIMITER ;
+
+
+-- requirement D: List Total Students Enrolled by Year
+DELIMITER $$
+CREATE PROCEDURE ListTotalStudentsEnrolledByYear()
+BEGIN
+    -- list total # of students enrolled by year
+    SELECT COUNT(sc.studentId) AS "Students", YEAR(sc.startdate) AS "Year"
+    FROM studentCourse AS sc
+    GROUP BY YEAR(sc.startdate)
+    ORDER BY YEAR(sc.startdate) ASC;
+END;
+$$
+DELIMITER ;
+
+
 
 -- call the stored procedures for each requirement
 
 -- requirement A
 CALL ListDepartmentCourses();
+
+-- requirement B
+CALL ListMostPopularCourses();
+
+-- requirement C-1
+CALL ListCoursesNoFaculty();
+
+-- requirement C-2
+CALL ListCourseStudentCountNoFaculty();
+
+-- requirement D
+CALL ListTotalStudentsEnrolledByYear();
 
 -- end of script
